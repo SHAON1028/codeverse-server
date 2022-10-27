396db671fd3440126436267678c641b89d8b921c
SHAON1028 <18101028@uap-bd.edu>
2022-10-27 23:06:56 +0600

    get path send res

--- index.js
+++ index.js
@@ -2,3 +2,29 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const port = process.env.PORT || 5000
+const courseData = require("./Data/courseData.json")
+
+app.use(cors())
+
+app.get('/courses',(req,res)=>{
+    res.send(courseData)
+})
+
+app.get('/course/:id',(req,res)=>{
+    const id = req.params.id
+    const singleCourseData = courseData.find(c=>c.id == id)
+    res.send(singleCourseData)
+})
+app.get('/checkout/:id',(req,res)=>{
+    const id = req.params.id
+    const checkoutData = courseData.find(c=>c.id == id)
+    res.send(checkoutData)
+})
+
+
+
+
+
+app.listen(port, ()=>{
+    console.log(`Dragon News Running on port ${port}`)
+})
